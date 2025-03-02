83e0986f95c1367a7141879b8f3f41f990833b32
hobbyscripterII <wruomma2@naver.com>
2025-03-02 12:38:20 +0900

    [2025-03-02] UPDATE - 차트 변경

--- js/chart.js
+++ js/chart.js
@@ -51,63 +51,8 @@ function getChartData(type) {
     const publicName = chartDataInfo.publicName;
     const valueSuffix = chartDataInfo.valueSuffix;
     const tickInterval = chartDataInfo.tickInterval;
+    const datasetData = chartDataInfo.data;
 
-    // dashboards-sync
-    /*
-    Highcharts.setOptions({
-        chart: {
-            type: 'area',
-            spacingTop: 20,
-            spacingBottom: 20,
-        },
-        title: {
-            align: 'left',
-            margin: 0,
-            x: 30
-        },
-        credits: {
-            enabled: false
-        },
-        legend: {
-            enabled: false
-        },
-        xAxis: {
-            categories: parseData.xData,
-            crosshair: true,
-            labels: {
-                format: '{value}'
-            },
-            accessibility: {
-                // description: 'Kilometers',
-                // rangeDescription: '0km to 6.5km'
-            }
-        },
-        yAxis: {
-            title: {
-                text: null
-            },
-            tickInterval: tickInterval
-        },
-        marker: {
-            enabled: false
-        },
-        tooltip: {
-            positioner: function () {
-                return {
-                    x: this.chart.chartWidth - this.label.width,
-                    y: 10
-                };
-            },
-            borderWidth: 0,
-            backgroundColor: 'none',
-            pointFormat: '{point.y}',
-            headerFormat: '',
-            shadow: false,
-            valueDecimals: 0
-        }
-    });
-    */
-    
     Dashboards.board('container', {
         dataPool: {
             connectors: [{
@@ -116,15 +61,14 @@ function getChartData(type) {
                 options: {
                     beforeParse: function (data) {
                         return [
-                            data.xData,
                             data.datasets.find(d => d.name === publicName).data,
+                            data.xData
                         ];
                     },
                     data: parseData,
-                    // dataUrl: 'https://www.highcharts.com/samples/data/activity.json',
                     firstRowAsNames: false,
                     orientation: 'columns',
-                    columnNames: ['x', publicName]
+                    columnNames: [publicName, 'x']
                 }
             }]
         },
@@ -157,7 +101,6 @@ function getChartData(type) {
         }]
     }, true);
 
-    // area-chart(demo)
     Highcharts.chart('container', {
        chart: {
             type: 'area'
@@ -166,6 +109,7 @@ function getChartData(type) {
             text: publicName
         },
         xAxis: {
+            type: 'category',
             categories: parseData.xData,
             crosshair: true,
             labels: {
@@ -183,17 +127,15 @@ function getChartData(type) {
             tickInterval: tickInterval
         },
         tooltip: {
-            pointFormat: '{series.name} had stockpiled <b>{point.y:,.0f}</b><br/>' +
-            'warheads in {point.x}',
+            pointFormat: '{series.name}: <b>{point.y}{series.userOptions.unit}</b>',
             valueSuffix: valueSuffix
         },
         plotOptions: {
             area: {
-                pointStart: 1940,
                 marker: {
                     enabled: false,
                     symbol: 'circle',
-                    radius: 2,
+                    radius: 1,
                     states: {
                         hover: {
                             enabled: true
@@ -203,18 +145,9 @@ function getChartData(type) {
             }
         },
         series: [{
-            /* name: 'USA' */
-            data: [
-                null, null, null, null, null, 2, 9, 13, 50, 170, 299, 438, 841,
-                1169, 1703, 2422, 3692, 5543, 7345, 12298, 18638, 22229, 25540,
-                28133, 29463, 31139, 31175, 31255, 29561, 27552, 26008, 25830,
-                26516, 27835, 28537, 27519, 25914, 25542, 24418, 24138, 24104,
-                23208, 22886, 23305, 23459, 23368, 23317, 23575, 23205, 22217,
-                21392, 19008, 13708, 11511, 10979, 10904, 11011, 10903, 10732,
-                10685, 10577, 10526, 10457, 10027, 8570, 8360, 7853, 5709, 5273,
-                5113, 5066, 4897, 4881, 4804, 4717, 4571, 4018, 3822, 3785, 3805,
-                3750, 3708, 3708, 3708, 3708
-            ]
+            name : publicName,
+            data : datasetData,
+            unit : valueSuffix
         }]
     });
 }
