58f7dc4f523c458bfbcdc5468ba21548632438bb
hobbyscripterII <wruomma2@naver.com>
2025-03-02 10:59:37 +0900

    [2025-03-02] MEMO - 차트 변경 작업중

--- js/chart.js
+++ js/chart.js
@@ -1,4 +1,6 @@
 function chagePlant(plant) {
+    plant = plant.trim();
+
     // 센서 데이터 초기화
     const sensorDataEl = $('.sensor-data');
     sensorDataEl.html('');
@@ -20,6 +22,22 @@ function chagePlant(plant) {
         sensorDataEl.append(el);
     });
 
+    // nav 효과
+    const subPlantEl = $('.sub-plant');
+
+    subPlantEl.each((idx, el) => {
+        // 기존 active 효과 제거
+        $(el).removeClass('active');
+
+        // 사용자가 키우고 있는 작물명
+        const text = $(el).text();
+
+        // 사용자가 클릭한 작물명이랑 일치할 경우 active 클래스 추가
+        if(plant == text) {
+            $(el).addClass('active');
+        }
+    });
+
     const defaultChartDataType = 'temp';
     getChartData(defaultChartDataType);
 }
@@ -34,6 +52,8 @@ function getChartData(type) {
     const valueSuffix = chartDataInfo.valueSuffix;
     const tickInterval = chartDataInfo.tickInterval;
 
+    // dashboards-sync
+    /*
     Highcharts.setOptions({
         chart: {
             type: 'area',
@@ -86,6 +106,7 @@ function getChartData(type) {
             valueDecimals: 0
         }
     });
+    */
     
     Dashboards.board('container', {
         dataPool: {
@@ -135,6 +156,67 @@ function getChartData(type) {
             }
         }]
     }, true);
+
+    // area-chart(demo)
+    Highcharts.chart('container', {
+       chart: {
+            type: 'area'
+        },
+        title: {
+            text: publicName
+        },
+        xAxis: {
+            categories: parseData.xData,
+            crosshair: true,
+            labels: {
+                format: '{value}'
+            },
+            accessibility: {
+                // description: 'Kilometers',
+                // rangeDescription: '0km to 6.5km'
+            }
+        },
+        yAxis: {
+            title: {
+                text: null
+            },
+            tickInterval: tickInterval
+        },
+        tooltip: {
+            pointFormat: '{series.name} had stockpiled <b>{point.y:,.0f}</b><br/>' +
+            'warheads in {point.x}',
+            valueSuffix: valueSuffix
+        },
+        plotOptions: {
+            area: {
+                pointStart: 1940,
+                marker: {
+                    enabled: false,
+                    symbol: 'circle',
+                    radius: 2,
+                    states: {
+                        hover: {
+                            enabled: true
+                        }
+                    }
+                }
+            }
+        },
+        series: [{
+            /* name: 'USA' */
+            data: [
+                null, null, null, null, null, 2, 9, 13, 50, 170, 299, 438, 841,
+                1169, 1703, 2422, 3692, 5543, 7345, 12298, 18638, 22229, 25540,
+                28133, 29463, 31139, 31175, 31255, 29561, 27552, 26008, 25830,
+                26516, 27835, 28537, 27519, 25914, 25542, 24418, 24138, 24104,
+                23208, 22886, 23305, 23459, 23368, 23317, 23575, 23205, 22217,
+                21392, 19008, 13708, 11511, 10979, 10904, 11011, 10903, 10732,
+                10685, 10577, 10526, 10457, 10027, 8570, 8360, 7853, 5709, 5273,
+                5113, 5066, 4897, 4881, 4804, 4717, 4571, 4018, 3822, 3785, 3805,
+                3750, 3708, 3708, 3708, 3708
+            ]
+        }]
+    });
 }
 
 // 더미 데이터
